a9c1d3ff10a5f23024aae5e29dfc57911ae9848c
unknown <johnshizhu@gmail.com>
2024-03-05 00:29:28 -0500

    slight updates

--- cumulative_topic_generation.py
+++ cumulative_topic_generation.py
@@ -19,4 +19,60 @@ initial_prompt = f"""
 
 proposal_obj = proposer.query(initial_prompt)
 proposal = proposal_obj.choices[0].message.content
+print("PROPOSAL--------------------")
+print(proposal)
+print("")
 
+verification_prompt = f"""
+    Task: Verify that the a previous proposal is valid given these constraints:
+    {constraints}
+
+    Previous Proposal:
+    {proposal}
+
+    Rate the proposal on a scale of 1-10
+"""
+verification_obj = verifier.query(verification_prompt)
+verification = verification_obj.choices[0].message.content
+print("")
+print("VERIFICATION --------------------------")
+print(verification)
+print("")
+
+iterations = 8
+
+for i in range(iterations):
+    proposal_prompt = f"""
+        Task: Generate a topic of interest given the following constraints:
+        {constraints}
+        This topic should following logically the previous topic which is this:
+        {proposal}
+        The new topic should be a deeper scope building on the previous topic. 
+        Only output the new topic.
+    """
+    prev_proposal = proposal
+    proposal_obj = proposer.query(initial_prompt)
+    proposal = proposal_obj.choices[0].message.content
+    print("PROPOSAL--------------------")
+    print(proposal)
+    print("")
+
+    verification_prompt = f"""
+        Task: Verify that the a target topic proposal is valid given these constraints:
+        {constraints}
+        Target proposal shoud logically follow from this previous proposal:
+        {prev_proposal}
+        
+        Target Proposal:
+        {proposal}
+
+        Rate the proposal on a scale of 1-10 based on how well it follows the constraint,
+        how well it logically follows the previous proposal and on if it does a good job achieving
+        a deep scope than the previous proposal
+    """
+    verification_obj = verifier.query(verification_prompt)
+    verification = verification_obj.choices[0].message.content
+    print("")
+    print("VERIFICATION --------------------------")
+    print(verification)
+    print("")
