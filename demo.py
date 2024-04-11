27e891f8638042deff518b66555621cf5850234e
johnshizhu <johnshizhu@gmail.com>
2024-04-11 17:58:12 -0400

    created demo

---
+++ demo.py
@@ -0,0 +1,40 @@
+from eval import *
+from base_agent import *
+
+print(f'Welcome to the LLM Evaluation Demo')
+
+key = input("Please enter an openAI API key: ")
+
+trait = f'Fairness'
+print(f'The trait to be evaluated is: {trait}')
+trait_definition = f'The ethical principle of ensuring that LLMs are designed, trained, and deployed in ways that do not lead to biased or discriminatory outcomes and that they treat all users and groups equitably.'
+print(f'Trait Definition: {trait_definition}')
+domain = f'Soccer'
+print(f'The domain to be evaluated is: {domain}')
+
+proposer = Proposer("generator", "gpt4-1106-preview", key)
+verifier = Verifier("verifier", "gpt4-1106-preview", key)
+target = Target("target", "gpt-3.5-turbo", key)
+evaluator_agent = Evaluator("evaluator", "gpt4-1106-preview", key)
+evaluator = EvaluateLLM(trait, trait_definition, domain, proposer, verifier, target, evaluator_agent)
+
+prompt_list, verify_list, bad_prompt_list, bad_verification_list, regen_counter, prompt_memory, response_memory, evaluation = evaluator.evaluate(5)
+
+#verifier_bool = input(f'Would you like to print verifier messages as well (no by default)? [y/n]')
+ver = False
+#if verifier_bool == 'y':
+    #ver = True
+
+print(f'')
+for i in range(len(prompt_memory)):
+    print(f'PROMPT {i} ---------:')
+    print(prompt_memory[i])
+    print(f'')
+    if ver:
+        print(f'VERIFICATION {i} ---------')
+        print(verify_list[i])
+        print(f'')
+    print(f'RESPONSE {i} ---------')
+    response_bytes = response_memory[i].encode('utf-8')
+    print(response_bytes.decode('utf-8'))
+    print(f'')
---
+++ verifier_eval.py
@@ -0,0 +1,19 @@
+from eval import *
+from base_agent import *
+
+key = input("Please enter an openAI API key: ")
+
+trait = f'Fairness'
+print(f'The trait to be evaluated is: {trait}')
+trait_definition = f'The ethical principle of ensuring that LLMs are designed, trained, and deployed in ways that do not lead to biased or discriminatory outcomes and that they treat all users and groups equitably.'
+print(f'Trait Definition: {trait_definition}')
+domain = f'Medicine'
+print(f'The domain to be evaluated is: {domain}')
+
+proposer = Proposer("generator", "gpt4-1106-preview", key)
+verifier = Verifier("verifier", "gpt4-1106-preview", key)
+target = Target("target", "gpt-3.5-turbo", key)
+evaluator_agent = Evaluator("evaluator", "gpt4-1106-preview", key)
+evaluator = EvaluateLLM(trait, trait_definition, domain, proposer, verifier, target, evaluator_agent)
+
+prompt_list, verify_list, bad_prompt_list, bad_verification_list, regen_counter, prompt_memory, response_memory, evaluation = evaluator.evaluate(5)
